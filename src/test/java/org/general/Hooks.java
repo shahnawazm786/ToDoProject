bbf353ca4eea78b0eea3b06ab026bc1381da2bc3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-01 20:14:41 +0530

    commit-3

--- src/test/java/org/general/Hooks.java
+++ src/test/java/org/general/Hooks.java
@@ -4,15 +4,13 @@ package org.general;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
 
 public class Hooks {
-static WebDriver driver;
+public static WebDriver driver;
     @BeforeSuite
 
     public void setupApps(){
--- src/test/java/org/utilities/ExplicitWaitManagment.java
+++ src/test/java/org/utilities/ExplicitWaitManagment.java
@@ -1,16 +1,33 @@
 package org.utilities;
 
-import org.general.Page1;
+import org.general.Hooks;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ExplicitWaitManagment {
 
     public static WebElement performanceExplicitWait(WaitStrategy waitStrategy, By by){
         WebElement element=null;
         if(waitStrategy==WaitStrategy.CLICKABLE){
-
+            element= new WebDriverWait(Hooks.driver, Duration.ofSeconds(20))
+                    .ignoring(NoSuchElementException.class)
+                    .until(ExpectedConditions.elementToBeClickable(by));
+        }
+        else if(waitStrategy==WaitStrategy.PERSENCE){
+            element= new WebDriverWait(Hooks.driver, Duration.ofSeconds(20))
+                    .ignoring(NoSuchElementException.class)
+                    .until(ExpectedConditions.visibilityOfElementLocated(by));
+        }
+        else if(waitStrategy==WaitStrategy.HANDLESTALEELEMENT){
+            element= new WebDriverWait(Hooks.driver, Duration.ofSeconds(20))
+                    .ignoring(NoSuchElementException.class)
+                    .until(ExpectedConditions.elementToBeClickable(by));
         }
         return element;
     }
