a4c6b3619975c5b623d50bb44e48764499fab435
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-01 20:19:38 +0530

    commit-4

---
+++ src/test/java/org/general/Page2.java
@@ -0,0 +1,20 @@
+package org.general;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Page2 {
+    WebDriver driver;
+    @Test
+    public void CommonTest(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://amazon.in");
+    }
+}
