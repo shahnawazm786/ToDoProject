ba286c894fcc4249bd290b148317d4f8bdc30464
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-01 14:12:39 +0530

    commit-3

--- src/test/java/org/utilities/ExplicitWaitManagment.java
+++ src/test/java/org/utilities/ExplicitWaitManagment.java
@@ -1,4 +1,17 @@
 package org.utilities;
 
+import org.general.Page1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class ExplicitWaitManagment {
+
+    public static WebElement performanceExplicitWait(WaitStrategy waitStrategy, By by){
+        WebElement element=null;
+        if(waitStrategy==WaitStrategy.CLICKABLE){
+
+        }
+        return element;
+    }
 }
