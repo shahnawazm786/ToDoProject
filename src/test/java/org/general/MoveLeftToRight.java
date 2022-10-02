6c09db79211718fb1b8c9a2ccc2bf3cc118fccab
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-02 10:18:46 +0530

    pom.xml

---
+++ src/test/java/org/general/MoveLeftToRight.java
@@ -0,0 +1,28 @@
+package org.general;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MoveLeftToRight {
+
+    WebDriver driver;
+    @Test
+    public void moveCursorLeftToRight(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://stackoverflow.com/questions/33094727/selenium-scroll-till-end-of-the-page");
+        //(//pre[@class='lang-js s-code-block'])[1]
+        WebElement element=driver.findElement(By.xpath("(//pre[@class='lang-js s-code-block'])[1]"));
+        //WebElement element=driver.findElement(By.xpath("//span[text()='var scroll =  document.documentElement.scrollHeight;window.scrollTo(0, scroll); return scroll;']"));
+        JavascriptExecutor jse=(JavascriptExecutor) driver;
+        //String script="arguments[0].scrollIntoView(true);";
+        String script="arguments[0].scrollBy(500,0)";
+        jse.executeScript(script,element);
+    }
+}
