43309e2823c2f307a0a53647ffbea3fc942a3463
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 22:20:19 +0530

    consumer

--- src/main/java/org/ConsumerClassExample.java
+++ src/main/java/org/ConsumerClassExample.java
@@ -2,6 +2,7 @@ package org;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Consumer;
 
 public class ConsumerClassExample {
@@ -13,8 +14,9 @@ public class ConsumerClassExample {
         //name.forEach(con);
         //name.forEach(c-> System.out.println(c));
         Consumer<String> con1=(c)->System.out.println(c.toUpperCase());
+        Consumer<String> con2=(c)-> System.out.println(c.toLowerCase());
         name.forEach(con1);
-        name.forEach(con.andThen(con1));
+        name.forEach(con.andThen(con1).andThen(con2));
 
     }
 }
