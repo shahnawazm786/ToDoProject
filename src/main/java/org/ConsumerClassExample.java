4713f3529209b803e2b05161a13a13c4232c3b84
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 21:50:50 +0530

    testng cucumber added

---
+++ src/main/java/org/ConsumerClassExample.java
@@ -0,0 +1,7 @@
+package org;
+
+public class ConsumerClassExample {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/org/steps/Hooks.java
+++ src/test/java/org/steps/Hooks.java
@@ -1,4 +1,18 @@
 package org.steps;
 
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Hooks {
+    @Before
+    public void setup(){
+        String browser=System.getenv("browser");
+        if(browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+        }
+        else if(browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+        }
+
+    }
 }
