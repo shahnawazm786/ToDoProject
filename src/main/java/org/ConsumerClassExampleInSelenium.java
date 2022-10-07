79e61b0ea8b1160f3137b2cc3632f038a5e02a20
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 22:33:00 +0530

    consumer

--- src/main/java/org/ConsumerClassExampleInSelenium.java
+++ src/main/java/org/ConsumerClassExampleInSelenium.java
@@ -1,14 +1,19 @@
 package org;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ConsumerClassExampleInSelenium {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.get("https://amazon.in");
+        List<WebElement> elements=driver.findElements(By.xpath("//a"));
         
     }
 }
