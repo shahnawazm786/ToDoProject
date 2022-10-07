379ad2952a044004d3ac3d1c878f2938de3f96e8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 23:15:51 +0530

    tentng framework

--- src/test/java/org/steps/MyStepdefs.java
+++ src/test/java/org/steps/MyStepdefs.java
@@ -7,9 +7,14 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class MyStepdefs {
     WebDriver driver=Hooks.driver;
+    List<WebElement> elements=null;
     @Given("search the link")
     public void searchTheLink() {
        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Vivo"+ Keys.ENTER);
@@ -17,7 +22,9 @@ public class MyStepdefs {
 
     @When("collect all text")
     public void collectAllText() {
-        System.out.println("Hello");
+        elements= driver.findElements(By.tagName("a"));
+        Consumer<WebElement> consumer=(el)-> System.out.println(el);
+        elements.forEach(consumer);
     }
 
     @Then("remove the duplicate text")
