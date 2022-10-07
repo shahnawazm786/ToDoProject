f31da337ce379a66120f1b2a297aa6e3754a70c9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 22:31:13 +0530

    consumer

--- src/main/java/org/ConsumerClassExampleInSelenium.java
+++ src/main/java/org/ConsumerClassExampleInSelenium.java
@@ -1,7 +1,13 @@
 package org;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ConsumerClassExampleInSelenium {
     public static void main(String[] args) {
-        
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
     }
 }
