2f1dc02132c709f69ce797f6cc9aadd3aecdefc1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 23:01:54 +0530

    consumer

--- src/test/java/org/steps/Hooks.java
+++ src/test/java/org/steps/Hooks.java
@@ -5,6 +5,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Hooks {
     public static WebDriver driver;
     @Before
@@ -18,5 +20,6 @@ public class Hooks {
         }*/
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 }
