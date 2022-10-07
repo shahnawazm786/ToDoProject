445dd3ed9f945fa65926aeafff09a8f99bedc9f6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-07 21:53:11 +0530

    testng cucumber added

--- src/main/java/org/ConsumerClassExample.java
+++ src/main/java/org/ConsumerClassExample.java
@@ -1,7 +1,15 @@
 package org;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ConsumerClassExample {
     public static void main(String[] args) {
+    List<String> name= Arrays.asList("Ahmad","Mohammad","Sakawat","Yaseer");
+        // lambda example
+        // consumer class
+        Consumer<String> con=(c)-> System.out.println(c);
 
     }
 }
