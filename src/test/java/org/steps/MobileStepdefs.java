0977e2a4a82815f4948df2431abca9d9e230ba0b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 13:41:09 +0530

    tentng framework

--- src/test/java/org/steps/MobileStepdefs.java
+++ src/test/java/org/steps/MobileStepdefs.java
@@ -1,4 +1,15 @@
 package org.steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
 public class MobileStepdefs {
+    @Given("user see the mobile link")
+    public void user_see_the_mobile_link(){
+
+    }
+    @When("user click on mobile link")
+    public void user_click_on_mobile_link(){
+        
+    }
 }
