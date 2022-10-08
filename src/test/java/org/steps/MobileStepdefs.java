aa20d48a7dbfb7d31d044bd443e8cb911f7e63e1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 14:49:24 +0530

    tentng framework

--- src/test/java/org/steps/MobileStepdefs.java
+++ src/test/java/org/steps/MobileStepdefs.java
@@ -19,5 +19,13 @@ public class MobileStepdefs {
     public void user_verify_the_category_title(){
     mobilePageObject.verifyCatoryText();
     }
+    @When("user click on mobile accessories click")
+    public void user_click_on_mobile_accessories_click(){
+
+    }
+    @Then("user verify explore by category")
+    public void user_verify_explore_by_category(){
+        
+    }
 
 }
