a105365367c9628a01693681e9ed405d8be29afb
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 14:17:30 +0530

    tentng framework

--- src/test/java/org/utilities/SeleniumCommonFunction.java
+++ src/test/java/org/utilities/SeleniumCommonFunction.java
@@ -1,17 +1,22 @@
 package org.utilities;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
 
 public class SeleniumCommonFunction {
     static WebDriverWait wait;
     public static boolean isElementDisplayed(WebDriver driver, By locator){
         wait=new WebDriverWait(driver,Duration.ofSeconds(20));
-        return true;
+        wait.ignoring(NoSuchElementException.class, IllegalStateException.class);
+        return wait.until(d->d.findElement(locator).isDisplayed());
     }
     public static void waitForElementVisibility(WebDriver driver, WebElement element){
 
