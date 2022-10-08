ca6b2d4ea0d01bb57b737e10886c3d814b8e44d4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 13:54:17 +0530

    tentng framework

--- src/test/java/org/page/MobilePageObject.java
+++ src/test/java/org/page/MobilePageObject.java
@@ -1,4 +1,10 @@
 package org.page;
 
+import org.openqa.selenium.WebDriver;
+
 public class MobilePageObject {
+    WebDriver driver;
+    public  MobilePageObject(WebDriver driver){
+        this.driver=driver;
+    }
 }
