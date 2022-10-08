ac8d6c9db2c5aa3898e6c2987ce97bb603588f17
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 14:12:19 +0530

    tentng framework

--- src/test/java/org/utilities/SeleniumCommonFunction.java
+++ src/test/java/org/utilities/SeleniumCommonFunction.java
@@ -3,13 +3,20 @@ package org.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SeleniumCommonFunction {
+    static WebDriverWait wait;
     public static boolean isElementDisplayed(WebDriver driver, By locator){
-
+        wait=new WebDriverWait(driver,Duration.ofSeconds(20));
         return true;
     }
     public static void waitForElementVisibility(WebDriver driver, WebElement element){
-        
+
+    }
+    public static void waitForElementVisibility(WebDriver driver, By locatory){
+
     }
 }
