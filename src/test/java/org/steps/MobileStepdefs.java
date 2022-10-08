662ad8dd772bc20590a62a530dd703ff8e9e9bdd
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 14:34:08 +0530

    tentng framework

--- src/test/java/org/steps/MobileStepdefs.java
+++ src/test/java/org/steps/MobileStepdefs.java
@@ -3,11 +3,13 @@ package org.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.page.MobilePageObject;
 
 public class MobileStepdefs {
+    MobilePageObject mobilePageObject=new MobilePageObject(Hooks.driver);
     @Given("user see the mobile link")
     public void user_see_the_mobile_link(){
-
+    mobilePageObject.veifyMobileLink();
     }
     @When("user click on mobile link")
     public void user_click_on_mobile_link(){
