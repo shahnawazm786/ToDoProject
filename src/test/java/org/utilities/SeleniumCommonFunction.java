999409459e3432415f15dfa64bc0e835ced2d4d3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 14:10:32 +0530

    tentng framework

--- src/test/java/org/utilities/SeleniumCommonFunction.java
+++ src/test/java/org/utilities/SeleniumCommonFunction.java
@@ -2,9 +2,14 @@ package org.utilities;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SeleniumCommonFunction {
     public static boolean isElementDisplayed(WebDriver driver, By locator){
+
         return true;
+    }
+    public static void waitForElementVisibility(WebDriver driver, WebElement element){
+        
     }
 }
