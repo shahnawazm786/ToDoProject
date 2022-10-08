ab908762d4d54342704cd331a4a09cec6135c29e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-08 14:01:50 +0530

    tentng framework

--- src/test/java/org/page/MobilePageObject.java
+++ src/test/java/org/page/MobilePageObject.java
@@ -1,6 +1,8 @@
 package org.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class MobilePageObject {
@@ -9,5 +11,10 @@ public class MobilePageObject {
         this.driver=driver;
         PageFactory.initElements(driver,this);
     }
+    // Element description
+    @FindBy(css = "a[data-csa-c-content-id='nav_cs_mobiles']")
+    private WebElement mobileLink;
+    @FindBy(xpath = "//span[text()='Category']")
+    private WebElement catgoryText;
     
 }
