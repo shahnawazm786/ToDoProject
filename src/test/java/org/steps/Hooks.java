b2ec3259d2c683e2ff14a2d585d341b6dd3a5a2c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-09 09:24:33 +0530

    tentng framework

--- src/test/java/org/steps/Hooks.java
+++ src/test/java/org/steps/Hooks.java
@@ -4,11 +4,14 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Hooks {
@@ -30,12 +33,15 @@ public class Hooks {
 
     }
     @After
-    public void cleanUp(Scenario scenario){
+    public void cleanUp(Scenario scenario) throws IOException {
         if(scenario.isFailed()){
-            String name=scenario.getName().replace(" ","_");
-            TakesScreenshot takesScreenshot=(TakesScreenshot) driver;
-            byte[] screen=takesScreenshot.getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screen,"img/png","error");
+        //    String name=scenario.getName().replace(" ","_");
+        //    TakesScreenshot takesScreenshot=(TakesScreenshot) driver;
+        //    byte[] screen=takesScreenshot.getScreenshotAs(OutputType.BYTES);
+        //    scenario.attach(screen,"img/png","error");
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            byte[] fileContent = FileUtils.readFileToByteArray(screenshot);
+            scenario.attach(fileContent, "image/png", "screenshot");
         }
         driver.manage().deleteAllCookies();
         driver.quit();
