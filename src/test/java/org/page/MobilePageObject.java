ce7f1e7b29d0a70b80432baae0581a0949bb21de
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-09 21:33:04 +0530

    tentng framework

--- src/test/java/org/page/MobilePageObject.java
+++ src/test/java/org/page/MobilePageObject.java
@@ -1,19 +1,27 @@
 package org.page;
 
+import jdk.internal.net.http.common.Log;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.utilities.SeleniumCommonFunction;
 
 import static org.testng.Assert.*;
 
 public class MobilePageObject {
     WebDriver driver;
+    final static Logger logger= LoggerFactory.getLogger(MobilePageObject.class);
     public  MobilePageObject(WebDriver driver){
+        logger.info("Object creation start----------------------");
         this.driver=driver;
         PageFactory.initElements(driver,this);
+        logger.info("Object created ----------------------");
+
     }
     // Element description
     @FindBy(css = "a[data-csa-c-content-id='nav_cs_mobiles']")
@@ -26,10 +34,14 @@ public class MobilePageObject {
     By exploreShopByCategoryText=By.cssSelector("//h2[text()='Shop by Category']");
     By exploreByCategoryText=By.xpath("//h2[text()='Explore by category']");
     public void veifyMobileLink(){
+        logger.info("Verify Mobile Link----------------------");
         assertEquals(true, SeleniumCommonFunction.isElementDisplayed(driver,mobileLink1));
+        logger.info("Mobile Link Verified ------------------------");
     }
     public void clickOnMobileLink(){
+        logger.info("Click on Mobile Link");
         driver.findElement(mobileLink1).click();
+        logger.info("Clicked on Mobile---------------");
     }
     public void verifyCatoryText(){
         assertEquals(true, SeleniumCommonFunction.isElementDisplayed(driver,catgoryText));
