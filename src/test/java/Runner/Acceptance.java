54b4f7b4a0a782bc777e0bedcd622e0b79b51393
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-09 09:04:21 +0530

    tentng framework

--- src/test/java/Runner/Acceptance.java
+++ src/test/java/Runner/Acceptance.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "org.steps",
-        tags = "@mobile",
+        tags = "@mobileaccessories",
         monochrome = true,
         plugin = {
         // "pretty"
