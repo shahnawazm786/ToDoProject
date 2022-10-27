59d71456a0d139f7e587346cb4f9457b72e259c5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-10-27 23:05:39 +0530

    searchTest

---
+++ src/main/java/org/FindDuplicateCharacter.java
@@ -0,0 +1,23 @@
+package org;
+
+import java.util.Scanner;
+
+public class FindDuplicateCharacter {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter String");
+        String str= scanner.nextLine();
+        System.out.println(str);
+        for(int i=0; i<str.length();i++){
+            for(int j=i+1;j<str.length();j++){
+                if(str.charAt(i)==str.charAt(j))
+                {
+                    System.out.print(str.charAt(i));
+                }
+            }
+        }
+
+    }
+}
+
+
