8c9c0fd822c9637288720129620f8003ed27bdb5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-02 23:32:31 +0530

    searchTest

--- src/main/java/excelutility/ExcelExample.java
+++ src/main/java/excelutility/ExcelExample.java
@@ -5,14 +5,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ExcelExample {
     XSSFWorkbook workbook;
     XSSFSheet sheet;
     FileInputStream inputStream;
-    final String filePath="C";
+    final String filePath="C:\\excel_file\\orders.xlsx";
     @Test
-    public void readExcelUsingLoop(){
+    public void readExcelUsingLoop() throws IOException{
+        inputStream=new FileInputStream(filePath);
+        workbook=new XSSFWorkbook(inputStream);
+
 
 
     }
