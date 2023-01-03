9303eb845b3f8731768d9f3a7be03abe42d68a7b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-03 23:20:35 +0530

    searchTest

--- src/main/java/excelutility/ExcelUtility.java
+++ src/main/java/excelutility/ExcelUtility.java
@@ -1,18 +1,22 @@
 package excelutility;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ExcelUtility {
     public Map<String,String> getData(String fileName,String sheetName) throws FileNotFoundException, IOException {
+        Map<String,String> map=new HashMap<>();
         FileInputStream inputStream=new FileInputStream(fileName);
         Workbook workbook=new XSSFWorkbook(inputStream);
-
-
+        Sheet sheet = workbook.getSheet(sheetName);
+        int lastRowNumber = sheet.getLastRowNum();
+        return map;
     }
 }
